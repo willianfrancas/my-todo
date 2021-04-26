193d5d23823d45e14742f4350ae90355fc7e0b60
willianfrancas <willianfrancas@gmail.com>
2021-04-26 20:20:53 -0300

    :fix: fixing routes and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,19 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './auth/auth.interceptor';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { MainModule } from './main/main.module';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
+import { HomeModule } from './home/home.module';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { FeedbackAlertComponent } from './shared/feedback-alert/feedback-alert.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,7 +31,9 @@ import { FeedbackAlertComponent } from './shared/feedback-alert/feedback-alert.c
     NoopAnimationsModule,
     AuthModule.forRoot(),
     AppRoutingModule,
+    HomeModule,
     MainModule,
+    CoreModule,
     SharedModule,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -2,20 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router'
 import { AuthGuard } from '../auth/auth-guard.service';
-import { ProfileComponent } from './profile/profile.component';
 import { MainComponent } from './main.component';
+import { ProfileComponent } from './profile/profile.component';
 import { CardMenuComponent } from './card-menu/card-menu.component';
 
 const routes: Routes = [
   {
-    path: 'home', component: MainComponent, canActivate: [AuthGuard],
+    path: '', component: MainComponent, canActivate: [AuthGuard],
     children: [
       { path: 'menu', component: CardMenuComponent, canActivate: [AuthGuard] },
       { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
     ]
-  }
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 ]
-
 @NgModule({
   declarations: [
   ],
